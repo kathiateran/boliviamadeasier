3afd3169ea58b6378cefddbcab6794f66da0313f
kathiateran <teran.kathia@gmail.com>
2020-11-20 12:07:20 -0500

    Bolivia Made Easier

---
+++ backend/data/products.js
@@ -0,0 +1,72 @@
+const products = [
+  {
+    name: 'Cycling the Death Road',
+    image: '/images/deathroad.jpg',
+    description:
+      'Summon your courage and conquer the ‘world’s most dangerous road’. Follow a professional guide downhill for 40 miles on a slender and serpentine road carved into the rainforest.',
+    brand: '',
+    category: 'Adventure',
+    price: 89.99,
+    countInStock: 7,
+    rating: 4.9,
+    numReviews: 22,
+  },
+  {
+    name: 'Explore Salar de Uyuni',
+    image: '/images/salardeuyuni.jpg',
+    description:
+      'A MUST see destination. This 3-day guided tour covers the most breathtaking salt flat sites: salt mounds, isolated ‘islands’ covered in cacti, colored lakes, hot springs, flamingo flocks, hot geysers, and more!',
+    brand: '',
+    category: 'Adventure & Sightseeing',
+    price: 199.99,
+    countInStock: 10,
+    rating: 4.8,
+    numReviews: 18,
+  },
+  {
+    name: 'Climb Mount Illimani!',
+    image: '/images/mountillimani.jpg',
+    description:
+      'Considered the guardian mountain of the city (technically it is an extinct stratovolcano!), Illimani stands at 6438 meters (21,122 ft) and it is the highest peak in the Cordillera Real. This is a challenging climb and while not very technical some experience is required. This climb lasts 3 days and runs only during the dry season.',
+    category: 'Adventure',
+    price: 549.99,
+    countInStock: 20,
+    rating: 4.6,
+    numReviews: 10,
+  },
+  {
+    name: 'Visit Sucre, ‘La Ciudad Blanca’',
+    image: '/images/sucre.jpg',
+    description:
+      'This trip is for the romantics! Visit the first capital of Bolivia (now a Unesco World Heritage Site), rich in colonial architecture and history.',
+    category: 'Sightseeing & Leisure',
+    price: 399.99,
+    countInStock: 11,
+    rating: 4.8,
+    numReviews: 12,
+  },
+  {
+    name: 'La Isla del Sol in Lake Titicaca',
+    image: '/images/isladelsol.jpg',
+    description:
+      'Birthplace of the sun and the Incan dynasty, Bolivia’s Isla del Sol is the embodiment of peace and serenity. Best explored at an ambling pace, be guided over 2 days through the island`s mystic sites.',
+    category: 'Sightseeing',
+    price: 149.99,
+    countInStock: 7,
+    rating: 3.9,
+    numReviews: 20,
+  },
+  {
+    name: 'El Valle de la Luna',
+    image: '/images/valleluna.jpg',
+    description:
+      'Take a guided tour of this natural wonder. Learn about the naturally-occurring phenomena of this site (creating impressive optical illusions on the eroded hillsides), and the folkloric mystical stories surrounding it!',
+    category: 'Sightseeing',
+    price: 29.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+  },
+]
+
+export default products
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To Bolivia Made Easier',
+  description: 'We offer the best tours in Bolivia!',
+  keywords: 'south america, south america tourism, bolivia tourism',
+}
+
+export default Meta
